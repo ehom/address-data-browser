c9ee26120cb7ab50050f489ff88a353492ba1c56
ehom <runner@fv-az121-16.n21guyhoirxuboad00cmdxewlc.cx.internal.cloudapp.net>
2020-10-24 02:49:00 +0000

    update generated file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var APP_NAME = "address data explorer";
+var Strings = {
+  appName: "address data explorer",
+  selectCountry: "select country",
+  outputForm: "output form",
+  inputForm: "input form"
+};
 
-document.title = APP_NAME;
+document.title = Strings.appName;
 
 function App(properties) {
   return React.createElement(
@@ -26,7 +31,7 @@ function App(properties) {
           React.createElement(
             "span",
             { className: "text-muted" },
-            "select country"
+            Strings.selectCountry
           )
         )
       ),
@@ -49,7 +54,7 @@ function App(properties) {
         React.createElement(
           "h4",
           null,
-          "input form"
+          Strings.inputForm
         ),
         React.createElement(
           "div",
@@ -68,7 +73,7 @@ function App(properties) {
         React.createElement(
           "h4",
           null,
-          "output form"
+          Strings.outputForm
         ),
         React.createElement(
           "div",
@@ -117,9 +122,8 @@ function CountrySelector(properties) {
         { value: code },
         properties.countries[code]['name']
       );
-    } else {
-      return React.createElement(React.Fragment, null);
     }
+    return React.createElement(React.Fragment, null);
   });
 
   return React.createElement(
